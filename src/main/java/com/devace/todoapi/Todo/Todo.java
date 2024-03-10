29494f0ca103a5c2dd8e1de1af160e3aa4ca73af
Ace Ladines <53437864+aceladines@users.noreply.github.com>
2024-03-10 18:25:13 +0800

    [Feature] - Added Todo entity (#6)
    * [Feature] - Added the initial controller for todos (#1)
    
    * [Feature] - Added exception for Todo not found
    
    Also created a dto response for errors
    
    * [Feature] - Created a todo entity
    
    * [Feature] - Update controller (#2)
    
    * [Feature] - Added the initial controller for todos
    
    * [Feature] - Added Todo Controller
    
    * [Feature] - Added service for Todo (#5)
    
    * [Feature] - Added TodoService interface
    
    * [Fix] - Module error

---
+++ src/main/java/com/devace/todoapi/Todo/Todo.java
@@ -0,0 +1,27 @@
+package com.devace.todoapi.Todo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotBlank
+    private String todo;
+
+    @NotBlank
+    @Pattern(regexp = "^(PENDING|COMPLETED|DELETED)$", message = "Status must be one of: PENDING, COMPLETED, DELETED")
+    private String status;
+}
